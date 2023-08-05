1e168b31e8f93ccc307df65c506ee0a6048e5afa
bernyig <bernardommguerreiro@gmail.com>
2023-08-05 20:26:30 +0200

    check for winner functionality

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function cellClicked() {
   }
 
   updateCell(this, cellIndex);
-  changePlayer();
+
   checkWinner();
 }
 
@@ -47,6 +47,33 @@ function changePlayer() {
   statusText.textContent = `${currentPlayer}'s turn`;
 }
 
-function checkWinner() {}
+function checkWinner() {
+  let roundWon = false;
+
+  for (let i = 0; i < winConditions.length; i++) {
+    const condition = winConditions[i];
+    const cellA = options[condition[0]];
+    const cellB = options[condition[1]];
+    const cellC = options[condition[2]];
+
+    if (cellA == "" || cellB == "" || cellC == "") {
+      continue;
+    }
+    if (cellA == cellB && cellB == cellC) {
+      roundWon = true;
+      break;
+    }
+  }
+
+  if (roundWon) {
+    statusText.textContent = `${currentPlayer} wins`;
+    running = false;
+  } else if (!options.includes("")) {
+    statusText.textContent = `Draw!`;
+    running = false;
+  } else {
+    changePlayer();
+  }
+}
 
 function restartGame() {}
